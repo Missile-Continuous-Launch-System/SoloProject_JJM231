fc7d79c44234e78d790333647d87bd5e25599ebf
JJJJJJJJJ <93594604+Realion033@users.noreply.github.com>
2023-05-25 16:41:07 +0900

    FIxx

--- SoloProject/Assets/Scripts/FPSController.cs
+++ SoloProject/Assets/Scripts/FPSController.cs
@@ -2,77 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSController: MonoBehaviour
+public class FPSController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 5f;
+    public float mouseSensitivity = 2f;
     public float jumpForce = 5f;
-    public float runMultiplier = 2f;
 
-    private float originalMoveSpeed;
-    private float originalRotationSpeed;
     private Rigidbody rb;
-    private bool isJumping = false;
-    private bool isRunning = false;
+    private Camera playerCamera;
 
-    private void Awake()
+    private float verticalRotation = 0f;
+    private bool isGrounded = true;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        originalMoveSpeed = moveSpeed;
-        originalRotationSpeed = rotationSpeed;
+        playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        // 플레이어 이동 처리
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
-        Vector3 movement = transform.right * horizontalMovement + transform.forward * verticalMovement;
+        // 이동 처리
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        if (isRunning)
-        {
-            movement *= moveSpeed * runMultiplier;
-        }
-        else
-        {
-            movement *= moveSpeed;
-        }
+        Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
+        rb.velocity = new Vector3(movement.x * moveSpeed, rb.velocity.y, movement.z * moveSpeed);
 
-        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+        // 마우스 시야 처리
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        // 플레이어 회전 처리
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
         transform.Rotate(Vector3.up * mouseX);
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
 
         // 점프 처리
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isJumping = true;
-        }
-
-        // 달리기 처리
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            isRunning = true;
-            moveSpeed = originalMoveSpeed * runMultiplier;
-            rotationSpeed = originalRotationSpeed * runMultiplier;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            isRunning = false;
-            moveSpeed = originalMoveSpeed;
-            rotationSpeed = originalRotationSpeed;
+            isGrounded = false;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // 바닥에 닿았을 때 점프 가능 상태로 초기화
+        // 바닥에 닿으면 점프 가능한 상태로 설정
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isJumping = false;
+            isGrounded = true;
         }
     }
 }
